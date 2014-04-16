089c28f41e94ff0103d377939581c065977abdad
Alex <exavior75@yahoo.com>
2014-04-15 22:52:26 -0700

    Update ansi.h

--- ansi.h
+++ ansi.h
@@ -1,15 +1,3 @@
-/*
-*********************************************************************************************************
-*                                                uC/OS-II
-*                                          The Real-Time Kernel
-*
-*                           (c) Copyright 1992-1999, Jean J. Labrosse, Weston, FL
-*                                           All Rights Reserved
-*
-*                                           MASTER INCLUDE FILE
-*********************************************************************************************************
-*/
-
 #include    <stdio.h>
 #include    <string.h>
 #include    <ctype.h>
